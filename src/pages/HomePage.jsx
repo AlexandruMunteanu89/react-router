631b0412e5b55a1bbb0daf0c92b1577b71e122e7
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-05-15 17:54:24 +0200

    Creata la barra di navigazione

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,21 @@ export default function HomePage(){
     return (
         <>
 
-            <header>Header</header>
-            <main>Main</main>
+            <header>
+                <nav class="navbar navbar-expand navbar-light bg-light">
+                    <div class="nav navbar-nav">
+                        <a class="nav-item nav-link active" href="#" aria-current="page">Home </a>
+                        <a class="nav-item nav-link" href="#">About</a>
+                        <a class="nav-item nav-link" href="#">Blog</a>
+                        <a class="nav-item nav-link" href="#">Contacts</a>
+                    </div>
+                </nav>
+                
+            </header>
+            <main>
+                
+                
+            </main>
             <footer>Footer</footer>
 
 
