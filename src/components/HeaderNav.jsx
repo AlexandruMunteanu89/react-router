b2a78f7c711c77c32250cd176f60d02cb682621d
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-05-20 14:00:53 +0200

    HeaderNav modifiche Css

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -2,10 +2,14 @@ import { Link, NavLink } from "react-router";
 
 export default function MainNav() {
     return (
-        <nav className="d-flex justify-content-start p-4 header-nav">
-            <NavLink className="p-2" to="/">Home Page</NavLink>
-            <NavLink className="p-2" to="/chi-siamo">Chi Siamo</NavLink>
-            <NavLink className="p-2" to="/prodotti">Prodotti</NavLink>
+        <nav>
+            <ul className="list-unstyled d-flex justify-content-end p-4 header-nav">
+                <li className="nav-item d-flex">
+                    <NavLink className='nav-link p-2' to="/">Home Page</NavLink>
+                    <NavLink className='nav-link p-2' to="/chi-siamo">Chi Siamo</NavLink>
+                    <NavLink className='nav-link p-2' to="/prodotti">Prodotti</NavLink>
+                </li>
+            </ul>
         </nav>
     );
 }
